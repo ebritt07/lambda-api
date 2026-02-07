ca2909ba8459958508f9b11c9c2cb2dc9a1dbe25
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2026-02-07 13:46:44 -0500

    dont expose id in base BikeDTO

--- python/src/main/api_gateway_schema/external_schema.py
+++ python/src/main/api_gateway_schema/external_schema.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, Field
 from src.main.lambdas.common.dynamo_schema import Bike, BikeStyle
 
 
-class BikeDTO(BaseModel, Bike):
+class BikeDTO(BaseModel):
     make: str = Field(..., example='Orbea')
     model: str
     style: BikeStyle
