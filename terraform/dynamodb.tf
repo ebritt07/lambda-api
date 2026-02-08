10d16b806ec69fe177b51a14d1fb2f4cd3929f89
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2026-02-07 19:39:31 -0500

    fixes

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "bikes" {
-  name         = "${var.org_name}-bikes"
+  name         = "BIKES"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "ID"
 
--- terraform/env/dev/variables.tfvars
+++ terraform/env/dev/variables.tfvars
@@ -1,3 +1,1 @@
 region             = "us-east-1"
-aws_account_id     = "862315107606"
-service_name       = "bicycle-lambdas"
