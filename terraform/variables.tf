43d48d42ab8200753d07fe395144ff1286ab1bb9
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2026-02-21 15:59:34 -0500

    Refactor API gateway config locals and file naming

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,16 +12,3 @@ variable "api_gateway_name" {
   type    = string
   default = "api-lambda-collection"
 }
-
-variable "api_gateway_contract_path" {
-  type    = string
-  default = "openapi/openapi.json"
-}
-
-variable "api_gateway_route_lambda_map" {
-  type = map(string)
-  default = {
-    "/admin" = "admin"
-    "/bike"  = "bicycle"
-  }
-}
