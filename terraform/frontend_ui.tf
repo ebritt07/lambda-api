ba39c13fa37352a33594b8e0103cea8ea5e45f62
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2026-02-22 15:24:06 -0500

    Allow Amplify to read frontend artifacts bucket

--- terraform/frontend_ui.tf
+++ terraform/frontend_ui.tf
@@ -12,6 +12,49 @@ resource "aws_s3_bucket" "frontend_artifacts" {
   bucket = "${local.org_name}-lambda-ui-artifacts-${var.env}"
 }
 
+data "aws_iam_policy_document" "frontend_artifacts_amplify_read" {
+  statement {
+    sid = "AllowAmplifyManualDeployRead"
+
+    principals {
+      type        = "Service"
+      identifiers = ["amplify.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetBucketLocation",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.frontend_artifacts.arn,
+    ]
+  }
+
+  statement {
+    sid = "AllowAmplifyManualDeployObjectRead"
+
+    principals {
+      type        = "Service"
+      identifiers = ["amplify.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.frontend_artifacts.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend_artifacts_amplify_read" {
+  bucket = aws_s3_bucket.frontend_artifacts.id
+  policy = data.aws_iam_policy_document.frontend_artifacts_amplify_read.json
+}
+
 resource "aws_s3_bucket_public_access_block" "frontend_artifacts" {
   bucket = aws_s3_bucket.frontend_artifacts.id
 
